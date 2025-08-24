4d3f82101c5d9b28eaf72232a3bf1385f044d6ab
Yasin <yasinpeker43@gmail.com>
2025-08-24 15:32:57 +0300

    feat(PostController) add HttpGet details

--- BlogProject/Controllers/PostsController.cs
+++ BlogProject/Controllers/PostsController.cs
@@ -3,25 +3,31 @@ using BlogProject.Data.Concrete.EfCore;
 using BlogProject.Entity;
 using BlogProject.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlogProject.Controllers;
 
 public class PostsController : Controller
 {
     private IPostRepository _postRepository;
-    
+
     public PostsController(IPostRepository postRepository)
     {
         _postRepository = postRepository;
-       
-        
+
+
     }
     public IActionResult Index()
     {
         return View(new PostViewModel
         {
             Posts = _postRepository.Posts.ToList(),
-           
+
         });
+    }
+    public async Task<IActionResult> Details(int? id)
+    {
+        return View(await _postRepository.Posts.FirstOrDefaultAsync(p => p.
+        PostId == id));
     }
 }
