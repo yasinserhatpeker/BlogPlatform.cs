40fb792384cc10c561f2c5e5d3ca6bc558ab8e87
Yasin <yasinpeker43@gmail.com>
2025-08-26 10:30:29 +0300

    feat(controller) add httpost login function

--- BlogProject/Controllers/UsersController.cs
+++ BlogProject/Controllers/UsersController.cs
@@ -9,14 +9,35 @@ namespace BlogProject.Controllers
 {
     public class UsersController : Controller
     {
-        public UsersController()
-        {
 
+        private IUserRepository _userRepository;
+        public UsersController(IUserRepository userRepository)
+        {
+           _userRepository=userRepository;
         }
 
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userRepository.Users.FirstOrDefaultAsync(x => x.Email == model.Email && x.Password == model.Password);
+
+                if (user != null)
+                {
+
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Email or password is incorrect.");
+                }
+            }
+             return View(model);
         }
     }
 
