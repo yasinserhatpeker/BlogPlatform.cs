75aeb36aeccf72564bdbb8944af974925815f6ad
Yasin <yasinpeker43@gmail.com>
2025-08-27 10:53:00 +0300

    fix(typo) fix typo

--- BlogProject/obj/Debug/net8.0/BlogProject.AssemblyInfo.cs
+++ BlogProject/obj/Debug/net8.0/BlogProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f36b7cc12a4ca2eb319b508b4276f899394bcfb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c77d1b0d46849c875b808a6e93d2050bba822888")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
